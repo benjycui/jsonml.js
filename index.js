483a68270fc44f077e1004f7369173a9a50b313d
Benjy Cui <benjytrys@gmail.com>
2016-04-06 10:44:30 +0800

    fix: should work in Node.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 
 const utils = require('./lib/utils');
 const dom = require('./lib/dom');
-const html = require('./html');
+const html = require('./lib/html');
 Object.assign(module.exports, utils, dom, html);
